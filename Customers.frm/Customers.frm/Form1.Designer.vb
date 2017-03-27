7d874d39f1ece9f59426089dfc08a54ac0f47faa
Callum <callum3gill@gmail.com>
2017-03-27 12:20:12 +0100

    VB Leisure center most recent

---
+++ Customers.frm/Customers.frm/Form1.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnStore = New System.Windows.Forms.Button()
+        Me.btnRetrieve = New System.Windows.Forms.Button()
+        Me.lbCustomerDetails = New System.Windows.Forms.ListBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(230, 64)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(142, 20)
+        Me.txtFirstName.TabIndex = 0
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(230, 90)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(142, 20)
+        Me.txtLastName.TabIndex = 1
+        '
+        'txtAddress
+        '
+        Me.txtAddress.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.txtAddress.Location = New System.Drawing.Point(230, 116)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(142, 20)
+        Me.txtAddress.TabIndex = 2
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(230, 142)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(142, 20)
+        Me.txtPhone.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(169, 67)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "First name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(169, 93)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Last name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(179, 119)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Address"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(147, 145)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(78, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Phone Number"
+        '
+        'btnStore
+        '
+        Me.btnStore.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStore.Location = New System.Drawing.Point(83, 185)
+        Me.btnStore.Name = "btnStore"
+        Me.btnStore.Size = New System.Drawing.Size(123, 57)
+        Me.btnStore.TabIndex = 8
+        Me.btnStore.Text = "Store details"
+        Me.btnStore.UseVisualStyleBackColor = True
+        '
+        'btnRetrieve
+        '
+        Me.btnRetrieve.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRetrieve.Location = New System.Drawing.Point(230, 185)
+        Me.btnRetrieve.Name = "btnRetrieve"
+        Me.btnRetrieve.Size = New System.Drawing.Size(122, 57)
+        Me.btnRetrieve.TabIndex = 9
+        Me.btnRetrieve.Text = "Retrive details"
+        Me.btnRetrieve.UseVisualStyleBackColor = True
+        '
+        'lbCustomerDetails
+        '
+        Me.lbCustomerDetails.FormattingEnabled = True
+        Me.lbCustomerDetails.Location = New System.Drawing.Point(37, 258)
+        Me.lbCustomerDetails.Name = "lbCustomerDetails"
+        Me.lbCustomerDetails.Size = New System.Drawing.Size(483, 160)
+        Me.lbCustomerDetails.TabIndex = 10
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(219, 424)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(121, 56)
+        Me.btnClear.TabIndex = 11
+        Me.btnClear.Text = "Clear list"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSearch.Location = New System.Drawing.Point(374, 186)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(110, 56)
+        Me.btnSearch.TabIndex = 12
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(165, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(277, 42)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "leasure Centre"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(630, 485)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lbCustomerDetails)
+        Me.Controls.Add(Me.btnRetrieve)
+        Me.Controls.Add(Me.btnStore)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPhone)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Name = "Form1"
+        Me.Text = "Leasure centre"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents txtLastName As System.Windows.Forms.TextBox
+    Friend WithEvents txtAddress As System.Windows.Forms.TextBox
+    Friend WithEvents txtPhone As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents btnStore As System.Windows.Forms.Button
+    Friend WithEvents btnRetrieve As System.Windows.Forms.Button
+    Friend WithEvents lbCustomerDetails As System.Windows.Forms.ListBox
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ Customers.frm/Customers.frm/Form1.vb
@@ -0,0 +1,64 @@
+﻿Imports System.IO
+Public Class Form1
+    Dim FirstName, LastName, Phone, Address As String
+    Dim ClientID As Integer
+    Private Structure CustomerDetails   'Record structure to store the values 
+        Public ClientID As Integer
+        Public FirstName As String
+        Public LastName As String
+        Public Address As String
+        Public PhoneNumber As String
+    End Structure
+    Private Sub btnStore_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStore.Click
+        FirstName = txtFirstName.Text
+        LastName = txtLastName.Text
+        Address = txtAddress.Text
+        Phone = LTrim(txtPhone.Text)   'Removes spaces on the left and right side of the values in the phone number textbox
+        Phone = RTrim(txtPhone.Text)
+        If txtFirstName.Text = "" Or txtLastName.Text = "" Then
+            MessageBox.Show("Please enter your first or last name")  'Checks to see if the users has entered their name and if not error message is outputed 
+            txtFirstName.BackColor = Color.Red
+            txtLastName.BackColor = Color.Red
+            Exit Sub
+        End If
+        If txtAddress.Text = "" Then
+            MessageBox.Show("Please enter your address")                     'Checks to see if the users has entered their age 
+            txtAddress.BackColor = Color.Red
+            Exit Sub
+        End If
+        If IsNumeric(txtPhone.Text) = False Then
+            MessageBox.Show("Please enter a valid phone number")             'Checks to see if the value in the phone number textbox is a numerical value 
+            txtPhone.BackColor = Color.Red
+            Exit Sub
+            If txtPhone.Text.Length > 11 Or txtPhone.Text.Length < 11 Then
+                MessageBox.Show("Please enter a valid phone number")         'Checks if the value in the phone number textbox is greater or less than 11
+                txtPhone.BackColor = Color.Red
+                Exit Sub
+            End If
+        End If
+        Dim CustomerDetails As New CustomerDetails
+        Dim sw As New StreamWriter(Application.StartupPath & "\Leisure.txt", True)    'Stores customer details in the textboxes in a textfile
+        CustomerDetails.FirstName = LSet(txtFirstName.Text, 30)
+        CustomerDetails.LastName = LSet(txtLastName.Text, 30)
+        CustomerDetails.Address = LSet(txtAddress.Text, 8)
+        CustomerDetails.PhoneNumber = LSet(txtPhone.Text, 11)
+        sw.WriteLine(CustomerDetails.FirstName & " " & CustomerDetails.LastName & " " & CustomerDetails.Address & " " & CustomerDetails.PhoneNumber)
+        sw.Close()
+        txtAddress.Clear()
+        txtFirstName.Clear()                'Clears all textboxes and then displays a message saying that the users details were successfully stored
+        txtLastName.Clear()
+        txtPhone.Clear()
+        MessageBox.Show("Your details have been stored")
+        txtPhone.BackColor = Color.White
+        txtAddress.BackColor = Color.White
+        txtFirstName.BackColor = Color.White
+        txtLastName.BackColor = Color.White
+    End Sub
+    Private Sub btnRetrieve_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRetrieve.Click
+        Dim lines() As String = IO.File.ReadAllLines(Dir$("Leisure.txt"))  'Retrives data from the text file and displays the data in a list box 
+        lbCustomerDetails.Items.AddRange(lines)                            'Adds items to the list box
+    End Sub
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        lbCustomerDetails.Items.Clear()            'Clears the list box 
+    End Sub
+End Class
