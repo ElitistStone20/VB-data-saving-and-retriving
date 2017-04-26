cb7a7f26485ad1932b9e13f7b7ca4220e701fe70
Callum <callum3gill@gmail.com>
2017-04-26 14:05:40 +0100

    VB Holidays 2

---
+++ Candidate-20170424T103311Z-001/Candidate/Holidays/Holidays/Holidays/Customers.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Customers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmdSave = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtPostcode = New System.Windows.Forms.TextBox()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtCustomerID = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmdRetrive = New System.Windows.Forms.Button()
+        Me.lbDetails = New System.Windows.Forms.ListBox()
+        Me.cmdClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmdSave
+        '
+        Me.cmdSave.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.cmdSave.Location = New System.Drawing.Point(93, 346)
+        Me.cmdSave.Name = "cmdSave"
+        Me.cmdSave.Size = New System.Drawing.Size(96, 32)
+        Me.cmdSave.TabIndex = 31
+        Me.cmdSave.Text = "Save"
+        Me.cmdSave.UseVisualStyleBackColor = False
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(56, 18)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(106, 29)
+        Me.lblName.TabIndex = 25
+        Me.lblName.Text = "Holidays"
+        '
+        'txtPostcode
+        '
+        Me.txtPostcode.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtPostcode.Location = New System.Drawing.Point(156, 158)
+        Me.txtPostcode.Name = "txtPostcode"
+        Me.txtPostcode.Size = New System.Drawing.Size(112, 20)
+        Me.txtPostcode.TabIndex = 24
+        '
+        'txtPhone
+        '
+        Me.txtPhone.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtPhone.Location = New System.Drawing.Point(156, 134)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(112, 20)
+        Me.txtPhone.TabIndex = 23
+        '
+        'txtLastName
+        '
+        Me.txtLastName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtLastName.Location = New System.Drawing.Point(156, 110)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(112, 20)
+        Me.txtLastName.TabIndex = 22
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtFirstName.Location = New System.Drawing.Point(156, 86)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(112, 20)
+        Me.txtFirstName.TabIndex = 21
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(66, 158)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(52, 13)
+        Me.Label5.TabIndex = 30
+        Me.Label5.Text = "Postcode"
+        '
+        'txtCustomerID
+        '
+        Me.txtCustomerID.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtCustomerID.Location = New System.Drawing.Point(156, 62)
+        Me.txtCustomerID.Name = "txtCustomerID"
+        Me.txtCustomerID.Size = New System.Drawing.Size(112, 20)
+        Me.txtCustomerID.TabIndex = 20
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(66, 134)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(38, 13)
+        Me.Label4.TabIndex = 29
+        Me.Label4.Text = "Phone"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(66, 110)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 13)
+        Me.Label3.TabIndex = 28
+        Me.Label3.Text = "LastName"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(66, 86)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 13)
+        Me.Label2.TabIndex = 27
+        Me.Label2.Text = "FirstName"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(66, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(62, 13)
+        Me.Label1.TabIndex = 26
+        Me.Label1.Text = "CustomerID"
+        '
+        'cmdRetrive
+        '
+        Me.cmdRetrive.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.cmdRetrive.Location = New System.Drawing.Point(204, 346)
+        Me.cmdRetrive.Name = "cmdRetrive"
+        Me.cmdRetrive.Size = New System.Drawing.Size(96, 32)
+        Me.cmdRetrive.TabIndex = 19
+        Me.cmdRetrive.Text = "Retrive"
+        Me.cmdRetrive.UseVisualStyleBackColor = False
+        '
+        'lbDetails
+        '
+        Me.lbDetails.FormattingEnabled = True
+        Me.lbDetails.Location = New System.Drawing.Point(81, 195)
+        Me.lbDetails.Name = "lbDetails"
+        Me.lbDetails.Size = New System.Drawing.Size(271, 121)
+        Me.lbDetails.TabIndex = 32
+        '
+        'cmdClear
+        '
+        Me.cmdClear.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.cmdClear.Location = New System.Drawing.Point(145, 384)
+        Me.cmdClear.Name = "cmdClear"
+        Me.cmdClear.Size = New System.Drawing.Size(96, 32)
+        Me.cmdClear.TabIndex = 33
+        Me.cmdClear.Text = "Clear data"
+        Me.cmdClear.UseVisualStyleBackColor = False
+        '
+        'Customers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(421, 420)
+        Me.Controls.Add(Me.cmdClear)
+        Me.Controls.Add(Me.lbDetails)
+        Me.Controls.Add(Me.cmdSave)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.txtPostcode)
+        Me.Controls.Add(Me.txtPhone)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtCustomerID)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmdRetrive)
+        Me.Name = "Customers"
+        Me.Text = "Customers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cmdSave As System.Windows.Forms.Button
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents txtPostcode As System.Windows.Forms.TextBox
+    Friend WithEvents txtPhone As System.Windows.Forms.TextBox
+    Friend WithEvents txtLastName As System.Windows.Forms.TextBox
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtCustomerID As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Private WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cmdRetrive As System.Windows.Forms.Button
+    Friend WithEvents lbDetails As System.Windows.Forms.ListBox
+    Friend WithEvents cmdClear As System.Windows.Forms.Button
+End Class
---
+++ Candidate-20170424T103311Z-001/Candidate/Holidays/Holidays/Holidays/Customers.vb
@@ -0,0 +1,74 @@
+﻿Imports System.IO
+Public Class Customers
+    Public Structure CustomerDetails
+        Public ID As Integer
+        Public FirstName As String
+        Public LastName As String
+        Public Phone As String
+        Public Postcode As String
+    End Structure
+    Private Sub Customers_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim ID As Integer
+        ID = CInt(txtCustomerID.Text)    'Converts the text in the textbox to an integer 
+    End Sub
+    Private Sub cmdSave_Click(sender As System.Object, e As System.EventArgs) Handles cmdSave.Click
+        Dim FirstName, LastName, Phone, Postcode As String
+        FirstName = txtFirstName.Text
+        LastName = txtLastName.Text
+        Phone = RTrim(txtPhone.Text)    'Removes any uneeded spaces so that the length check will work properally and therefore the user cant fill up the textbox with spaces 
+        Phone = LTrim(txtPhone.Text)
+        Postcode = txtPostcode.Text.ToUpper   'Converts the characters entered in the textbox to upper case so that the postcode is accurate when being saved 
+        If txtFirstName.Text = "" Then
+            MsgBox("Please enter your first name")   'Presence check to see if the user has entered their firstname so that when saving an error wont occur 
+            Exit Sub
+        ElseIf txtLastName.Text = "" Then
+            MsgBox("Please enter your last name")     'Presence check to see if the user has entered their last name so that an error wont occur when trying to save values which arent there
+        ElseIf txtPhone.Text = "" Then
+            MsgBox(" Please enter your phone number")    'presence check to see if the user has entered their phoen number
+            Exit Sub
+        Else
+            If IsNumeric(txtPhone.Text) = False Then
+                MsgBox("Please enter a valid phone number")   'Data type check to see if the value entered by the user is a numerical value and if not then an error message is output
+                Exit Sub
+            ElseIf txtPhone.Text.Length < 11 Or txtPhone.Text.Length > 11 Then   'Length check to see if the users number entered is 11 characters long which is the length of a phone number
+                MsgBox("Please enter a valid phone number")
+                Exit Sub
+            End If
+        End If
+        Try
+            Dim CustomerData As New CustomerDetails                             'Declares  custoemr data as the values in the record structure CustomerDetails
+            Dim sw As New System.IO.StreamWriter("CustoemrDetails.txt", True)
+            CustomerData.ID = LSet(txtCustomerID.Text, 50)
+            CustomerData.FirstName = LSet(txtFirstName.Text, 50)
+            CustomerData.LastName = LSet(txtLastName.Text, 50)
+            CustomerData.Phone = LSet(txtPhone.Text, 11)
+            CustomerData.Postcode = LSet(txtPostcode.Text, 6)
+            txtCustomerID.Text = txtCustomerID.Text + 1
+            sw.WriteLine(CustomerData.ID & " " & CustomerData.FirstName & " " & CustomerData.LastName & " " & CustomerData.Phone & "" & CustomerData.Postcode)   'Writes all the custoemr details to the text file and then outputs a message sayingt that the file has saved 
+            sw.Close()
+            MsgBox("Details saved")
+            txtFirstName.Clear()
+            txtLastName.Clear()  'Clears all the textboxes except the Id textbox as that is going to be added by one 
+            txtPhone.Clear()
+            Dim CustomerID() As String = File.ReadAllLines("CustomerDetails.txt")
+            For i = 1 To UBound(CustomerID)
+                If Trim(Mid(CustomerID(i), 1, 50)) = txtCustomerID.Text Then   'If the ID in the customer ID textbox equals the ID in the record structure then the value in the textbox is incremented by one 
+                    txtCustomerID.Text = Str(CustomerID(i)) + 1
+                End If
+            Next i
+        Catch ex As Exception
+            MessageBox.Show("There was a problem saving the details please try again later")  'If an error occurs when saving the customer details then a message is output telling the user to try again 
+        End Try
+    End Sub
+    Private Sub cmdRetrive_Click(sender As System.Object, e As System.EventArgs) Handles cmdRetrive.Click
+        Dim CustomerData() As String = File.ReadAllLines("CustomerDetails.txt")
+        For i = 1 To UBound(CustomerData)
+            If Trim(Mid(CustomerData(i), 1, 50)) = txtCustomerID.Text Then   'If the ID in the text file is equal to the ID input then the details are retrived and added to the listbox 
+                lbDetails.Items.AddRange(CustomerData)
+            End If
+        Next i
+    End Sub
+    Private Sub cmdClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdClear.Click
+        lbDetails.Items.Clear()    'Clears the details in the listbox 
+    End Sub
+End Class
